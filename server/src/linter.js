aa89ca3b946f3744b6799f4d66d5c78a77be9753
Saw-jan <saw.jan.grg3e@gmail.com>
2022-03-20 18:40:21 +0545

    add validation for invalid line

--- server/src/linter.js
+++ server/src/linter.js
@@ -1,21 +1,20 @@
 const { DiagnosticSeverity } = require('vscode-languageserver/node');
-const { removeCommentsTags } = require('./utils');
+const { replaceCommentsTags, replaceDocString, replaceStory, getLineKeyword } = require('./utils');
 const defaultSettings = require('./defaultSettings');
 const Keywords = require('./keywords');
-
-const stepKeywords = ['Given', 'When', 'Then', 'And', 'But'];
-const exampleKeywords = ['Background', 'Scenario', 'Example', 'Scenario Outline', 'Scenario Template'];
 
 module.exports.validateDocument = function (document, docConfig) {
     const diagnostics = [];
     validateFeature(document, diagnostics);
     validateBeginningStep(document, diagnostics, docConfig);
-    checkRepeatedSteps(document, diagnostics);
+
+    const simpleText = getSimpleText(document);
+    checkRepeatedSteps(document, simpleText, diagnostics);
     return diagnostics;
 };
 
 function validateFeature(document, diagnostics) {
-    let text = document.getText();
+    const text = document.getText();
     const regex = /(?<!\S( )*)Feature:/g;
     let matchCount = 0;
     while ((match = regex.exec(text))) {
@@ -28,7 +27,7 @@ function validateFeature(document, diagnostics) {
                     start: document.positionAt(match.index),
                     end: document.positionAt(match.index + match[0].length),
                 },
-                message: 'There must be only one "Feature" keyword in a file.',
+                message: `There must be only one "${Keywords.Feature}" keyword in a file.`,
             });
         }
     }
@@ -45,9 +44,9 @@ function validateFeature(document, diagnostics) {
 }
 
 function validateBeginningStep(document, diagnostics, docConfig) {
-    let text = removeCommentsTags(document.getText());
+    const text = replaceCommentsTags(document.getText());
     const regex =
-        /(?<=(Background:|Scenario( (Outline|Template))?:|Example:)(.*)[\n\r](\s)*)[GWTAB]{1}[ivenhdut]{2,4}/g;
+        /(?<=(Background:|Scenario( (Outline|Template))?:|Example:)(.*)[\n\r](\s)*)(Given|When|Then|And|But)( )/g;
 
     let noOfProblem = 0;
     const maxNumberOfProblems = docConfig.maxNumberOfProblems || defaultSettings.maxNumberOfProblems;
@@ -59,21 +58,21 @@ function validateBeginningStep(document, diagnostics, docConfig) {
                 severity: DiagnosticSeverity.Warning,
                 range: {
                     start: document.positionAt(match.index),
-                    end: document.positionAt(match.index + match[0].length),
+                    end: document.positionAt(match.index + match[0].length - 1),
                 },
-                message: 'Starting step must be "Given" or "When" step',
+                message: `Starting step must be "${Keywords.Given}" or "${Keywords.When}" step`,
             });
         }
     }
 }
 
-function checkRepeatedSteps(document, diagnostics) {
-    let text = removeCommentsTags(document.getText());
+function checkRepeatedSteps(document, text, diagnostics) {
     const lines = text.split('\n');
-    const regex = /[GWTAB]{1}[ivenhdut]{2,4}/;
+    const regex = /(Given|When|Then|And|But)/;
 
     let prevStep = '';
     let index = 0;
+    let keywordHit = false;
     lines.forEach((line) => {
         let lineLength = line.length;
         if (lineLength === 0) {
@@ -88,14 +87,40 @@ function checkRepeatedSteps(document, diagnostics) {
             index += lineLength;
             return;
         }
+
+        const keyword = getLineKeyword(line);
+        if (!keyword) {
+            diagnostics.push({
+                severity: DiagnosticSeverity.Error,
+                range: {
+                    start: document.positionAt(index),
+                    end: document.positionAt(index + line.length),
+                },
+                message: 'Invalid line',
+            });
+
+            index += lineLength;
+            return;
+        } else if (!keywordHit && keyword !== Keywords.Feature) {
+            diagnostics.push({
+                severity: DiagnosticSeverity.Error,
+                range: {
+                    start: document.positionAt(index),
+                    end: document.positionAt(index + line.length),
+                },
+                message: 'A valid feature file must start with a Feature name.\nE.g., Feature: Login feature',
+            });
+        }
+        keywordHit = true;
+
         const match = regex.exec(line);
         if (Boolean(match)) {
-            const matchStep = match[0].trim().trim(':');
+            const matchStep = match[0];
             if (prevStep === matchStep) {
                 const rangeEnd = index + match.index + matchStep.length;
-                let message = `Replace "${matchStep}" with "And"`;
-                if (matchStep !== 'But') {
-                    message += ' or "But"';
+                let message = `Replace "${matchStep}" with "${Keywords.And}"`;
+                if (matchStep !== Keywords.But) {
+                    message += ` or "${Keywords.But}"`;
                 }
                 diagnostics.push({
                     severity: DiagnosticSeverity.Warning,
@@ -106,17 +131,20 @@ function checkRepeatedSteps(document, diagnostics) {
                     message,
                 });
             }
-            if (matchStep !== 'And') {
+            if (matchStep !== Keywords.And) {
                 prevStep = matchStep;
             }
-            if (!stepKeywords.includes(matchStep)) {
-                prevStep = '';
-            }
         } else {
-            console.log(line);
+            prevStep = '';
         }
         index += lineLength;
     });
 }
 
-function isUnknownLine(line) {}
+// return text where tags, comments, user story and docstring are replaced with space
+function getSimpleText(document) {
+    let text = replaceCommentsTags(document.getText());
+    text = replaceDocString(text);
+    text = replaceStory(text);
+    return text;
+}
--- server/src/utils.js
+++ server/src/utils.js
@@ -1,4 +1,4 @@
-function removeCommentsTags(text) {
+function replaceCommentsTags(text) {
     const lines = text.split('\n');
     let newText = [];
     lines.forEach((line) => {
@@ -11,6 +11,37 @@ function removeCommentsTags(text) {
     return newText.join('\n');
 }
 
+function replaceDocString(text) {
+    const regex = /"""[\s\S]*?"""/g;
+    while ((match = regex.exec(text))) {
+        const hiddenDocString = match[0].replace(/\S/g, ' ');
+        text = text.substring(0, match.index) + hiddenDocString + text.substring(match.index + hiddenDocString.length);
+    }
+    return text;
+}
+
+function replaceStory(text) {
+    const regex = /(?<=Feature:.*[\s])[\s]*[Aa]s (.*)[\s]*[Ii] want (.*)[\s]*[Ss]o that (.*)/;
+    const match = regex.exec(text);
+    const hiddenDocString = match[0].replace(/\S/g, ' ');
+    text = text.substring(0, match.index) + hiddenDocString + text.substring(match.index + hiddenDocString.length);
+
+    return text;
+}
+
+function getLineKeyword(line) {
+    const regex =
+        /^(\t| )*((Feature:|Rule:|Background:|Scenario:|Example:|Scenario Outline:|Scenario Template:|Examples:|Scenarios:|#)( )?|@|"""|(Given|When|Then|And|But)( )|\|[\S\t ]*\|)/;
+    const match = regex.exec(line);
+    if (match) {
+        return match[0].trim().replace(':', '');
+    }
+    return null;
+}
+
 module.exports = {
-    removeCommentsTags,
+    replaceCommentsTags,
+    replaceDocString,
+    replaceStory,
+    getLineKeyword,
 };
